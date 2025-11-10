c995610071fb9e01847c1b3765a0281baa008620
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:28:40 +0100

    Java Programming I

--- part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
+++ part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
@@ -2,10 +2,6 @@
 public class AdaLovelace {
 
     public static void main(String[] args) {
-        // Write your program here
-<<<<<<< HEAD
-=======
         System.out.println("Ada Lovelace");
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 }
