d0f9646e95aa2f564c189b4eb4a7d595766d88ce
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:39:42 +0100

    Java Programming I

--- part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
+++ part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -6,14 +6,11 @@ public class SpeedingTicket {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here. 
-<<<<<<< HEAD
-=======
+      
         System.out.println("Give speed: ");
         int a = scanner.nextInt();
         if (a>120){
             System.out.println("Speeding ticket!");
         }
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 }
