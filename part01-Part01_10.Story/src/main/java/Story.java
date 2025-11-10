3b3e27df3fcf1ad42ef2129f6d80f637b4ef7b15
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:31:32 +0100

    Java Programming I

--- part01-Part01_10.Story/src/main/java/Story.java
+++ part01-Part01_10.Story/src/main/java/Story.java
@@ -5,10 +5,6 @@ public class Story {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-<<<<<<< HEAD
-=======
         System.out.println("I will tell you a story, but I need some information first.\n" +
 "What is the main character called?");
         String name = scanner.nextLine();
@@ -18,7 +14,6 @@ public class Story {
 "Once upon a time there was " +name+ ", who was "+job +".\n" +
 "On the way to work, "+name+ " reflected on life.\n" +
 "Perhaps "+name+ " will not be "+job+ " forever.");
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
 
     }
 }
