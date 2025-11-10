9d4ccd9a9c4fa7789ef338ae28e906d846eda009
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:30:30 +0100

    Java Programming I

--- part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
+++ part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
@@ -5,14 +5,11 @@ public class MessageThreeTimes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Write a message:");
-        // Write your program here
-<<<<<<< HEAD
+       
 
     }
 }
-=======
         String message = scanner.nextLine();
         for (int i=0; i<3; i++){
             System.out.println(message);
@@ -20,4 +17,3 @@ public class MessageThreeTimes {
     }
 }
 }
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
