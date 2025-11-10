8ebb126963a25b0a6c1a8de07cff3b662b875837
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:30:48 +0100

    Java Programming I

--- part01-Part01_08.Greeting/src/main/java/Greeting.java
+++ part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -6,13 +6,8 @@ public class Greeting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
-<<<<<<< HEAD
-
-=======
         System.out.println("What's your name? ");
         String name = scanner.nextLine();
         System.out.println("Hi " +name);
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 }
