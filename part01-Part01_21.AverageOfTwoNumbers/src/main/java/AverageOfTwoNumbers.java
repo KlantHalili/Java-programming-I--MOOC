2f1abd437eed2ed4cd0fdec2057122e382a43a0f
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:38:48 +0100

    Java Programming I

--- part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -5,17 +5,11 @@ public class AverageOfTwoNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-<<<<<<< HEAD
-
-=======
         System.out.println("Give the first number:");
         int a = Integer.valueOf(scanner.nextLine());
         System.out.println("Give the second number:");
         int b = Integer.valueOf(scanner.nextLine());
         double average = (double)(a+b)/2;
         System.out.println("The average is "+average);
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 }
