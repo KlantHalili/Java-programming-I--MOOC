490455d4246ef42a8c39df66df2567d6dda0666a
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:41:29 +0100

    Java Programming I

--- part01-Part01_27.Ancient/src/main/java/Ancient.java
+++ part01-Part01_27.Ancient/src/main/java/Ancient.java
@@ -4,11 +4,7 @@ import java.util.Scanner;
 public class Ancient {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        Scanner scan = new Scanner(System.in);
 
-        // Write your program here
-=======
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
@@ -17,8 +13,5 @@ public class Ancient {
         if (a < 2015){
             System.out.println("Ancient history");
         }
-        
-        
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 }
