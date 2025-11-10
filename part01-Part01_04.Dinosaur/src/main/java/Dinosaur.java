1bf858ad5a358d34f432de358b7bf38c2dcb2e25
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:29:25 +0100

    Java Programming I

--- part01-Part01_04.Dinosaur/src/main/java/Dinosaur.java
+++ part01-Part01_04.Dinosaur/src/main/java/Dinosaur.java
@@ -2,12 +2,9 @@
 public class Dinosaur {
 
     public static void main(String[] args) {
-        // Write your program here
-<<<<<<< HEAD
-=======
+        
         System.out.println("Once upon a time");
         System.out.println("there was");
         System.out.println("a dinosaur");
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 }
