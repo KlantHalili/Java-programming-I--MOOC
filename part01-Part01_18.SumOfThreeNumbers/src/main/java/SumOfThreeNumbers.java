bb044006bc9b73fab733479ed7c578e50c244b6f
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:37:08 +0100

    Java Programming I

--- part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
+++ part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
@@ -6,10 +6,6 @@ public class SumOfThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
-<<<<<<< HEAD
-
-=======
         System.out.println("Give the first number:");
         int a = Integer.valueOf(scanner.nextLine());
         System.out.println("Give the second number:");
@@ -17,6 +13,5 @@ public class SumOfThreeNumbers {
         System.out.println("Give the third number:");
         int c = Integer.valueOf(scanner.nextLine());
         System.out.println("The sum of the numbers is " + (a + b + c));
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 }
