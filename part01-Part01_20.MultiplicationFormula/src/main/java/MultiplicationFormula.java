08d27c3ed5656b829c582b1486d55a46920b0b6f
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:38:27 +0100

    Java Programming I

--- part01-Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
+++ part01-Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
@@ -3,13 +3,6 @@ import java.util.Scanner;
 
 public class MultiplicationFormula {
 
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-
-=======
      public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -19,6 +12,5 @@ public class MultiplicationFormula {
         System.out.println("Give the second number:");
         int b = Integer.valueOf(scanner.nextLine());
         System.out.println(a + " * " + b + " = " + (a*b));
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 }
