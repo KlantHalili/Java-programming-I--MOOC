bacb758a6f11652aa77425be504ac42d720ac492
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:32:36 +0100

    Java Programming I

--- part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
+++ part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
-
-import java.util.Scanner;
-
-public class IntegerInput {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // write your program here
-
-=======
 import java.util.Scanner;
  
 public class IntegerInput {
  
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
- 
-        // write your program here
         System.out.println("Give a number:");
         int number = Integer.valueOf(scanner.nextLine());
         System.out.println("You gave the number " + number);
  
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 }
