0c24d4e28bcba86497ae5d1cdefdcaafb48d5bb7
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:28:07 +0100

    Update Sandbox.java

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -2,11 +2,7 @@
 public class Sandbox {
 
     public static void main(String[] args) {
-        // Write your program here
-<<<<<<< HEAD
-=======
         System.out.println("I fundit");
         System.out.println("kshu kshu");
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 }
