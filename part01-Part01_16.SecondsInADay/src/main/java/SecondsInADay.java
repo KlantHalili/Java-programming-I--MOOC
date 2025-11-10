03322dd008f36119331500f46bcb4601614a5a6d
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:36:27 +0100

    Java Programming I

--- part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -5,15 +5,10 @@ public class SecondsInADay {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-<<<<<<< HEAD
-=======
         System.out.println("How many days would you like to convert to seconds?");
         int a = scanner.nextInt();
         int b = a * 86400;
         System.out.println(b);
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
 
     }
 }
