b78cc76d24e01455820490fcee71c88aed32255e
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:34:39 +0100

    Java Programming I

--- part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
+++ part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
@@ -1,23 +1,15 @@
-
-import java.util.Scanner;
-
-public class DoubleInput {
-
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // write your program here
-
-=======
-      public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
- 
-        // write your program here
-        System.out.println("Give a number:");
-        double number = Double.valueOf(scanner.nextLine());
-        System.out.println("You gave the number " + number);
- 
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
-    }
-}
+	 
+	import java.util.Scanner;
+	 
+	public class DoubleInput {
+	 
+	      public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	 
+	        // write your program here
+	        System.out.println("Give a number:");
+	        double number = Double.valueOf(scanner.nextLine());
+	        System.out.println("You gave the number " + number);
+	 
+	    }
+	}
