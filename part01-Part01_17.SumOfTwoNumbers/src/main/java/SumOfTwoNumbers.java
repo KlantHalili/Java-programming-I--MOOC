8926a187e717b0d96759f8f277cebc3e8f52c71f
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:36:49 +0100

    Java Programming I

--- part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
+++ part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
@@ -6,15 +6,10 @@ public class SumOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
-<<<<<<< HEAD
-
-=======
         System.out.println("Give the first number:");
         int a = Integer.valueOf(scanner.nextLine());
         System.out.println("Give the second number:");
         int b = Integer.valueOf(scanner.nextLine());
         System.out.println("The sum of the numbers is " + (a + b));
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 }
