b4854c3d62df676f96c532ad53d4342f6e655eef
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:30:08 +0100

    Java Programming I

--- part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
+++ part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -3,11 +3,7 @@ public class HiAdaLovelace {
 
     public static void main(String[] args) {
         String name = "Ada Lovelace";
-<<<<<<< HEAD
-=======
         System.out.println("Hi " + name + "!");
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
-
 
     }
 }
