6ab66fb3807b9638eb798a15202d8c8195fe0c70
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:29:43 +0100

    Java Programming I

--- part01-Part01_05.Message/src/main/java/Message.java
+++ part01-Part01_05.Message/src/main/java/Message.java
@@ -7,14 +7,10 @@ public class Message {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Write a message:");
-<<<<<<< HEAD
-=======
         
         String message = scanner.nextLine();
         
         System.out.println(message);
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
-        // Write your program here
 
     }
 }
