7d3df003a53f3eba45a26a45365b9bca25a2bdea
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-10 17:41:04 +0100

    Java Programming I

--- part01-Part01_26.Orwell/src/main/java/Orwell.java
+++ part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -4,11 +4,7 @@ import java.util.Scanner;
 public class Orwell {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        Scanner scan = new Scanner(System.in);
 
-        // Write your program here
-=======
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
@@ -17,8 +13,5 @@ public class Orwell {
         if (a == 1984){
             System.out.println("Orwell");
         }
-        
-        
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 }
