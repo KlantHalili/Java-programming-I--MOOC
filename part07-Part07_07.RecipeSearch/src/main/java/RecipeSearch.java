b14df51e004323a8d97e396038734d2b4672fc85
Klanti Halili <80346320+KlantHalili@users.noreply.github.com>
2025-11-23 16:50:58 +0100

    Java Programming I

--- part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
+++ part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
@@ -7,12 +7,8 @@ public class RecipeSearch {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-<<<<<<< HEAD
-
-=======
         UserInterface ui = new UserInterface(scanner);
         ui.Start();
->>>>>>> 0ccf91eb9230efe2a8bf702228293770df8f45df
     }
 
 }
